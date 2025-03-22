cbc521c517ad6e50b2b156a9cff40cdf25ba6db0
nyashka <oleshakirsanova@gmail.com>
2025-03-22 11:47:44 +0200

    Main

---
+++ MainPage/scrypt.js
@@ -0,0 +1,31 @@
+const hamMenu = document.querySelector('.burgMenu')
+const offScreenMenu = document.querySelector ('.off-screen-menu')
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active'),
+    offScreenMenu.classList.toggle('active')
+
+})
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll(".link");
+
+window.addEventListener("mousemove", (e) => {
+  
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  cursor.forEach(el => {
+    el.style.left = `${x}px`;
+    el.style.top = `${y}px`;
+    
+    links.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+        el.classList.add("hover");
+      })
+      link.addEventListener("mouseleave", () => {
+        el.classList.remove("hover");
+      })
+    })
+    
+  })
+  
+})
