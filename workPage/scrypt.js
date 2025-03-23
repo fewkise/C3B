73512f60c0cc516d85c4a4291acbec54845bfb96
nyashka <oleshakirsanova@gmail.com>
2025-03-23 14:07:26 +0200

    MerchPageadding

--- workPage/scrypt.js
+++ workPage/scrypt.js
@@ -5,3 +5,27 @@ hamMenu.addEventListener('click', () => {
     offScreenMenu.classList.toggle('active')
 
 })
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll(".link, .button");
+
+window.addEventListener("mousemove", (e) => {
+  
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  cursor.forEach(el => {
+    el.style.left = `${x}px`;
+    el.style.top = `${y}px`;
+    
+    links.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+        el.classList.add("hover");
+      })
+      link.addEventListener("mouseleave", () => {
+        el.classList.remove("hover");
+      })
+    })
+    
+  })
+  
+})
